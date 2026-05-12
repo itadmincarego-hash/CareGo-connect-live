068b4ef408ddd8064a88a1a56b8482904700869a
itadmincarego-hash <itadmincarego@gmail.com>
2026-05-12 15:26:14 +0100

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -8,6 +8,7 @@ export const getRouter = () => {
 
   const router = createRouter({
     routeTree,
+    basepath: "/carewise-connect-live/",
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
